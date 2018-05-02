91664c6872587c48fffa66e0f7efda441e0e2c41
Eric <gazrighian_eric@yaho.fr>
2018-05-02 17:55:55 +0000

    Move tfstate into s3

--- autoscaling/autoscaling.tf
+++ autoscaling/autoscaling.tf
@@ -2,6 +2,14 @@ provider "aws" {
   region = "eu-west-3"
 }
 
+terraform {
+  backend "s3" {
+    bucket = "eg2-s3bucket-for-shared-terraform-tfstate"
+    key    = "autoscaling/terraform.tfstate"
+    region = "eu-west-3"
+  }
+}
+
 resource "aws_autoscaling_group" "my-asg" {
   launch_configuration = "${aws_launch_configuration.my-launch-config.id}"
 
