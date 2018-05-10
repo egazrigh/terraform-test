33cf9d96dd0b8538ecf4614bae28d384dd54d6d1
Eric <gazrighian_eric@yaho.fr>
2018-05-10 12:02:38 +0000

    User data small changes

--- Consolidated/modules/services/webserver-cluster/main.tf
+++ Consolidated/modules/services/webserver-cluster/main.tf
@@ -103,7 +103,7 @@ resource "aws_security_group" "elb_sg" {
   name = "${var.cluster_name}-elb_sg"
 }
 
-resource "aws_security_group_rule" "allow_http_in" {
+resource "aws_security_group_rule" "allow_http_in_on_elb" {
   type              = "ingress"
   security_group_id = "${aws_security_group.elb_sg.id}"
   from_port         = 80
@@ -112,7 +112,7 @@ resource "aws_security_group_rule" "allow_http_in" {
   cidr_blocks       = ["0.0.0.0/0"]
 }
 
-resource "aws_security_group_rule" "allow_all_out" {
+resource "aws_security_group_rule" "allow_all_out_on_elb" {
   type              = "egress"
   security_group_id = "${aws_security_group.elb_sg.id}"
   from_port         = 0
--- Consolidated/modules/services/webserver-cluster/user-data.sh
+++ Consolidated/modules/services/webserver-cluster/user-data.sh
@@ -2,7 +2,7 @@
 cat > index.html <<EOF 
 <H1> "Hello World" </H1>
 <p>DB address : ${db_address}</p>
-<p>DB port    : ${db_port}" </p>
+<p>DB port    : ${db_port}</p>
 EOF
 nohup busybox httpd -f -p "${server_port}" &
               
